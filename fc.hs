c8ecd43d055311e534b8a7490c083d4ce7bc4fcd
Brian Johnson (Mac Pro) <bjthinks@gmail.com>
2010-10-10 16:06:41 -0700

    Refactor: handle each input line in a function

--- fc.hs
+++ fc.hs
@@ -5,22 +5,24 @@ import Tokenizer
 import Expression
 import ExpressionParser
 
+main :: IO ()
+main = do setCompletionEntryFunction $ Just $ \_ -> return []
+          mainloop
+
 mainloop :: IO ()
 mainloop = do i <- readline "> "
               case i of
                 Nothing -> return ()
-                Just str -> do if str /= ""
-                                 then addHistory str
-                                 else return ()
-                               case parseAll tokenizer str of
-                                 Right tokens -> do case parseAll expressionParser tokens of
-                                                      Right expr -> putStrLn $ show expr
-                                                      Left err -> putStrLn "Error: unrecognized expression"
-                                                    mainloop
-                                 Left err -> do putStrLn $ "  " ++ replicate (errorLocation err) '-' ++ "^"
-                                                putStrLn "Error: unrecognized input"
-                                                mainloop
+                Just str -> do processLine str
+                               mainloop
 
-main :: IO ()
-main = do setCompletionEntryFunction $ Just $ \_ -> return []
-          mainloop
+processLine :: String -> IO ()
+processLine str = do if str /= ""
+                       then addHistory str
+                       else return ()
+                     case parseAll tokenizer str of
+                       Right tokens -> do case parseAll expressionParser tokens of
+                                            Right expr -> putStrLn $ show expr
+                                            Left err -> putStrLn "Error: unrecognized expression"
+                       Left err -> do putStrLn $ "  " ++ replicate (errorLocation err) '-' ++ "^"
+                                      putStrLn "Error: unrecognized input"
