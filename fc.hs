124a53a2c31a6a307e49a29d523f06167e1a60cb
Brian Johnson (Mac Pro) <bjthinks@gmail.com>
2010-10-10 02:16:36 -0700

    Tab completion does not match filenames anymore. In fact, it no longer matches anything at all...

--- fc.hs
+++ fc.hs
@@ -1,7 +1,11 @@
 import System.Console.Readline
 
+mainloop :: IO ()
+mainloop = do i <- readline "> "
+              case i of
+                Nothing -> return ()
+                Just str -> addHistory str >> main
+
 main :: IO ()
-main = do i <- readline "> "
-          case i of
-            Nothing -> return ()
-            Just str -> addHistory str >> main
+main = do setCompletionEntryFunction $ Just $ \_ -> return []
+          mainloop
