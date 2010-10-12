4aeb581c8ac5b5e7997e9d1482b9a0d13abc24a7
Brian Johnson (MacBook Pro) <bjthinks@gmail.com>
2010-10-12 04:58:58 -0700

    Words are tokens

--- Tokenizer.hs
+++ Tokenizer.hs
@@ -3,11 +3,18 @@ module Tokenizer (Token(..), isInteger, tokenizer) where
 import Parser
 import Data.Char
 
-data Token = TokenInteger Integer | TokenMinus deriving (Eq, Show)
+data Token = TokenInteger Integer |
+             TokenWord String |
+             TokenMinus
+             deriving (Eq, Show)
 
 isInteger :: Token -> Bool
 isInteger (TokenInteger _) = True
 isInteger _ = False
+
+isWord :: Token -> Bool
+isWord (TokenWord _) = True
+isWord _ = False
 
 tokenizer :: Parser Char [(Int,Token)]
 tokenizer = do ts <- pStar (spaces >> token)
@@ -20,15 +27,17 @@ spaces = do pStar $ pProp isSpace
 
 token :: Parser Char (Int,Token)
 token = do n <- numParsed
-           t <- integer ||| minus
+           t <- integer ||| word ||| minus
            return (n,t)
 
 integer :: Parser Char Token
-integer = do ds <- digits
+integer = do ds <- pPlus $ pProp isDigit
              return $ TokenInteger (read ds :: Integer)
 
-digits :: Parser Char [Char]
-digits = pPlus $ pProp isDigit
+word :: Parser Char Token
+word = do c <- pProp isAlpha
+          cs <- pStar $ pProp isAlphaNum
+          return $ TokenWord (c:cs)
 
 minus :: Parser Char Token
 minus = do pElt '-'
