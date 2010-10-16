bbc44fb66009d4270a001f8a7040ddbdf2005778
Brian Johnson (Mac Pro) <bjthinks@gmail.com>
2010-10-16 14:58:08 -0700

    Product printed with constants first

--- ExpressionDisplay.hs
+++ ExpressionDisplay.hs
@@ -12,10 +12,19 @@ displayWithPrecedence (ExpressionSum (e:es)) =
   (foldl withplus (displayExpr e) es,0) where
     withplus :: String -> Expression -> String
     withplus s t = s ++ " + " ++ displayExpr t
-displayWithPrecedence (ExpressionProduct (e:es)) =
-  (foldl withspace (parenthesize (displayWithPrecedence e) 1) es,1) where
-    withspace :: String -> Expression -> String
-    withspace s t = s ++ " " ++ parenthesize (displayWithPrecedence t) 1
+displayWithPrecedence (ExpressionProduct es) =
+  (foldl withspace f fs,1) where
+    (f:fs) = map displayTerm intFirstExprs
+    intFirstExprs = filter isConstant es ++ filter isNonConstant es
+    displayTerm :: Expression -> String
+    displayTerm e = parenthesize (displayWithPrecedence e) 1
+    withspace :: String -> String -> String
+    withspace s t = s ++ " " ++ t
+    isConstant :: Expression -> Bool
+    isConstant (ExpressionInteger _) = True
+    isConstant _ = False
+    isNonConstant :: Expression -> Bool
+    isNonConstant = not . isConstant
 
 parenthesize :: (String,Int) -> Int -> String
 parenthesize (str,x) y
