9eec2a83bd8be25089ab379a99669ec75e4fb48b
Brian Johnson (MacBook Pro) <bjthinks@gmail.com>
2010-10-19 12:05:16 -0700

    Subtraction handled better in printing

--- ExpressionDisplay.hs
+++ ExpressionDisplay.hs
@@ -17,9 +17,11 @@ displayInteger n = (show n,10)
 displayVariable v = (v,10)
 
 displaySum es =
-  (foldl withplus f fs,0) where
-    withplus :: String -> String -> String
-    withplus s t = s ++ " + " ++ t
+  (foldl withspace f fs',0) where
+    fs' = map expandSign fs
+    expandSign :: String -> String
+    expandSign ('-':cs) = '-':' ':cs
+    expandSign cs = '+':' ':cs
     (f:fs) = map displayExpr intLastExprs
     intLastExprs = filter isNonConstant es ++ filter isConstant es
 
@@ -28,13 +30,14 @@ displayProduct es = let
   (f:fs) = map displayTerm (filter isNonConstant es)
   displayTerm :: Expression -> String
   displayTerm e = parenthesize (displayWithPrecedence e) 1
-  withspace :: String -> String -> String
-  withspace s t = s ++ " " ++ t
   in case filter isConstant es of
     [] -> (rest,1)
     [c] -> case eMatch id (error "") (error "") (error "") c of
       (-1) -> ("-" ++ rest,1)
       cc -> (show cc ++ " " ++ rest,1)
+
+withspace :: String -> String -> String
+withspace s t = s ++ " " ++ t
 
 parenthesize :: (String,Int) -> Int -> String
 parenthesize (str,x) y
