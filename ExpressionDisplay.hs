ba10abb21f43b685b1ebd6088fcabaa023b18908
Brian Johnson (MacBook Pro) <bjthinks@gmail.com>
2010-10-19 11:53:05 -0700

    Products with negative numbers in them display more correctly

--- ExpressionDisplay.hs
+++ ExpressionDisplay.hs
@@ -13,28 +13,45 @@ displayWithPrecedence = eMatch
                         displayProduct
 
 displayInteger n = (show n,10)
+
 displayVariable v = (v,10)
+
 displaySum es =
   (foldl withplus f fs,0) where
     withplus :: String -> String -> String
     withplus s t = s ++ " + " ++ t
     (f:fs) = map displayExpr intLastExprs
     intLastExprs = filter isNonConstant es ++ filter isConstant es
-    fTrue x = True
-    fFalse x = False
-    isConstant :: Expression -> Bool
-    isConstant = eMatch fTrue fFalse fFalse fFalse
-    isNonConstant :: Expression -> Bool
-    isNonConstant = not . isConstant
-displayProduct es =
-  (foldl withspace f fs,1) where
-    (f:fs) = map displayTerm es
-    displayTerm :: Expression -> String
-    displayTerm e = parenthesize (displayWithPrecedence e) 1
-    withspace :: String -> String -> String
-    withspace s t = s ++ " " ++ t
+
+displayProduct es = let
+  rest = foldl withspace f fs
+  (f:fs) = map displayTerm (filter isNonConstant es)
+  displayTerm :: Expression -> String
+  displayTerm e = parenthesize (displayWithPrecedence e) 1
+  withspace :: String -> String -> String
+  withspace s t = s ++ " " ++ t
+  in case filter isConstant es of
+    [] -> (rest,1)
+    [c] -> case eMatch id (error "") (error "") (error "") c of
+      (-1) -> ("-" ++ rest,1)
+      cc -> (show cc ++ " " ++ rest,1)
 
 parenthesize :: (String,Int) -> Int -> String
 parenthesize (str,x) y
   | x < y = "(" ++ str ++ ")"
   | otherwise = str
+
+isConstant :: Expression -> Bool
+isConstant = eMatch fTrue fFalse fFalse fFalse
+
+isNonConstant :: Expression -> Bool
+isNonConstant = not . isConstant
+
+fTrue :: a -> Bool
+fTrue = first True
+
+fFalse :: a -> Bool
+fFalse = first False
+
+first :: a -> b -> a
+first x _ = x
