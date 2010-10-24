dbbd2015166048c99988471b2fe6fc9b051bca5f
Brian Johnson (MacBook Pro) <bjthinks@gmail.com>
2010-10-23 22:06:47 -0700

    Moved tokenizer tests into Tokenizer module

--- Tokenizer.hs
+++ Tokenizer.hs
@@ -1,7 +1,12 @@
-module Tokenizer (Token(..), isInteger, isWord, tokenizer) where
+module Tokenizer (Token(..),
+                  isInteger,
+                  isWord,
+                  tokenizer,
+                  test_Tokenizer) where
 
 import Parser
 import Data.Char
+import Test.HUnit
 
 data Token = TokenInteger Integer |
              TokenWord String |
@@ -67,3 +72,48 @@ power  = pElt '^' >> return TokenPower
 openParen  = pElt '(' >> return TokenOpenParen
 closeParen = pElt ')' >> return TokenCloseParen
 assign = pElt ':' >> pElt '=' >> return TokenAssign
+
+test_Tokenizer :: Test
+test_Tokenizer = test [
+  isLeft (parseAll tokenizer "{") ~?= True,
+  isLeft (parseAll tokenizer "abc 123 +-*/ @") ~?= True,
+  parseAll tokenizer ""        ~?= Right [(0,TokenEnd)],
+  parseAll tokenizer "1"       ~?= Right [(0,TokenInteger 1),(1,TokenEnd)],
+  parseAll tokenizer "123"     ~?= Right [(0,TokenInteger 123),(3,TokenEnd)],
+  parseAll tokenizer " 1"      ~?= Right [(1,TokenInteger 1),(2,TokenEnd)],
+  parseAll tokenizer "   456"  ~?= Right [(3,TokenInteger 456),(6,TokenEnd)],
+  parseAll tokenizer " 1 2 3 " ~?= Right [(1,TokenInteger 1),
+                                          (3,TokenInteger 2),
+                                          (5,TokenInteger 3),
+                                          (7,TokenEnd)],
+  parseAll tokenizer "    345    5678   34    " ~?=
+  Right [(4,TokenInteger 345),
+         (11,TokenInteger 5678),
+         (18,TokenInteger 34),
+         (24,TokenEnd)],
+  parseAll tokenizer "-"       ~?= Right [(0,TokenMinus),(1,TokenEnd)],
+  parseAll tokenizer " - 9 "   ~?= Right [(1,TokenMinus),
+                                          (3,TokenInteger 9),
+                                          (5,TokenEnd)],
+  parseAll tokenizer "---"     ~?=
+  Right [(0,TokenMinus),(1,TokenMinus),(2,TokenMinus),(3,TokenEnd)],
+  parseAll tokenizer "abc"     ~?= Right [(0,TokenWord "abc"),(3,TokenEnd)],
+  parseAll tokenizer "ABC"     ~?= Right [(0,TokenWord "ABC"),(3,TokenEnd)],
+  parseAll tokenizer "a23"     ~?= Right [(0,TokenWord "a23"),(3,TokenEnd)],
+  parseAll tokenizer " a11 11" ~?=
+  Right [(1,TokenWord "a11"),(5,TokenInteger 11),(7,TokenEnd)],
+  parseAll tokenizer ":="      ~?= Right [(0,TokenAssign),(2,TokenEnd)],
+  parseAll tokenizer "3-:=-3"  ~?=
+  Right [(0,TokenInteger 3),(1,TokenMinus),(2,TokenAssign),
+         (4,TokenMinus),(5,TokenInteger 3),(6,TokenEnd)],
+  parseAll tokenizer "+"       ~?= Right [(0,TokenPlus),(1,TokenEnd)],
+  parseAll tokenizer "("       ~?= Right [(0,TokenOpenParen),(1,TokenEnd)],
+  parseAll tokenizer ")"       ~?= Right [(0,TokenCloseParen),(1,TokenEnd)],
+  parseAll tokenizer "*"       ~?= Right [(0,TokenTimes),(1,TokenEnd)],
+  parseAll tokenizer "/"       ~?= Right [(0,TokenDivide),(1,TokenEnd)],
+  parseAll tokenizer "^"       ~?= Right [(0,TokenPower),(1,TokenEnd)]
+  ]
+  where
+    isLeft :: Either a b -> Bool
+    isLeft (Left _) = True
+    isLeft (Right _) = False
--- testfc.hs
+++ testfc.hs
@@ -10,37 +10,6 @@ import Expand
 
 (tRat,tVar,tSum,tProd,tIntPow) = useThisVariableOnlyForTestingTheExpressionConstructors
 
-tokenizerTests = test [
-  isLeft (parseAll tokenizer "{") ~?= True,
-  isLeft (parseAll tokenizer "abc 123 +-*/ @") ~?= True,
-  parseAll tokenizer ""        ~?= Right [(0,TokenEnd)],
-  parseAll tokenizer "1"       ~?= Right [(0,TokenInteger 1),(1,TokenEnd)],
-  parseAll tokenizer "123"     ~?= Right [(0,TokenInteger 123),(3,TokenEnd)],
-  parseAll tokenizer " 1"      ~?= Right [(1,TokenInteger 1),(2,TokenEnd)],
-  parseAll tokenizer "   456"  ~?= Right [(3,TokenInteger 456),(6,TokenEnd)],
-  parseAll tokenizer " 1 2 3 " ~?= Right [(1,TokenInteger 1),(3,TokenInteger 2),(5,TokenInteger 3),(7,TokenEnd)],
-  parseAll tokenizer "    345    5678   34    " ~?= Right [(4,TokenInteger 345),(11,TokenInteger 5678),(18,TokenInteger 34),(24,TokenEnd)],
-  parseAll tokenizer "-"       ~?= Right [(0,TokenMinus),(1,TokenEnd)],
-  parseAll tokenizer " - 9 "   ~?= Right [(1,TokenMinus),(3,TokenInteger 9),(5,TokenEnd)],
-  parseAll tokenizer "---"     ~?= Right [(0,TokenMinus),(1,TokenMinus),(2,TokenMinus),(3,TokenEnd)],
-  parseAll tokenizer "abc"     ~?= Right [(0,TokenWord "abc"),(3,TokenEnd)],
-  parseAll tokenizer "ABC"     ~?= Right [(0,TokenWord "ABC"),(3,TokenEnd)],
-  parseAll tokenizer "a23"     ~?= Right [(0,TokenWord "a23"),(3,TokenEnd)],
-  parseAll tokenizer " a11 11" ~?= Right [(1,TokenWord "a11"),(5,TokenInteger 11),(7,TokenEnd)],
-  parseAll tokenizer ":="      ~?= Right [(0,TokenAssign),(2,TokenEnd)],
-  parseAll tokenizer "3-:=-3"  ~?= Right [(0,TokenInteger 3),(1,TokenMinus),(2,TokenAssign),(4,TokenMinus),(5,TokenInteger 3),(6,TokenEnd)],
-  parseAll tokenizer "+"       ~?= Right [(0,TokenPlus),(1,TokenEnd)],
-  parseAll tokenizer "("       ~?= Right [(0,TokenOpenParen),(1,TokenEnd)],
-  parseAll tokenizer ")"       ~?= Right [(0,TokenCloseParen),(1,TokenEnd)],
-  parseAll tokenizer "*"       ~?= Right [(0,TokenTimes),(1,TokenEnd)],
-  parseAll tokenizer "/"       ~?= Right [(0,TokenDivide),(1,TokenEnd)],
-  parseAll tokenizer "^"       ~?= Right [(0,TokenPower),(1,TokenEnd)]
-  ]
-
-isLeft :: Either a b -> Bool
-isLeft (Left _) = True
-isLeft (Right _) = False
-
 unRight :: Either a b -> b
 unRight (Right b) = b
 unRight _ = error "parse failed"
@@ -346,7 +315,7 @@ storeTests = test [
   getValue "a" (setValue "a" (tVar "x") (setValue "b" (tRat 11) newStore)) ~?= Just (tVar "x")
   ]
 
-tests = test ["tokenizer" ~: tokenizerTests,
+tests = test ["tokenizer" ~: test_Tokenizer,
               "expression parser" ~: expressionParserTests,
               "expression display" ~: expressionDisplayTests,
               "store" ~: storeTests,
