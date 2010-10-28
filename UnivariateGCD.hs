b3102c3c143a669f274be6b738dd229132937fc8
Brian Johnson (Mac Pro) <bjthinks@gmail.com>
2010-10-27 23:30:47 -0700

    Added scratch work on getting polynomial gcd to work.  This is stalled waiting for a book from Amazon that will help me address the coefficient problem.

---
+++ UnivariateGCD.hs
@@ -0,0 +1,85 @@
+module UnivariateGCD where
+
+import Data.Ratio (Rational)
+import PrimeList
+import Test.HUnit
+
+type Term = (Integer,Integer)
+
+-- Polynomial must be passed as a list of (exponent,coefficient)
+-- pairs.
+-- FIXME: Need to sanitize the input.
+univariateGCD :: [(Integer,Rational)] -> [(Integer,Rational)] ->
+                 [(Integer,Rational)]
+univariateGCD f g = undefined
+
+reducePolyMod :: Integer -> [Term] -> [Term]
+reducePolyMod p = removeZeroTerms . map (reduceTermMod p)
+
+reduceTermMod :: Integer -> Term -> Term
+reduceTermMod p (e,c) = (e,mod c p)
+
+removeZeroTerms :: [Term] -> [Term]
+removeZeroTerms ((_,0):ts) = removeZeroTerms ts
+removeZeroTerms (t:ts) = t : removeZeroTerms ts
+removeZeroTerms [] = []
+
+multiplyTerms :: Term -> Term -> Term
+multiplyTerms (e1,c1) (e2,c2) = (e1+e2,c1*c2)
+
+multiplyTermByPoly :: Term -> [Term] -> [Term]
+multiplyTermByPoly t = map (multiplyTerms t)
+
+multiplyTermByPolyMod :: Integer -> Term -> [Term] -> [Term]
+multiplyTermByPolyMod p t ts = reducePolyMod p $ multiplyTermByPoly t ts
+
+degree :: [Term] -> Integer
+degree [] = (-1)
+degree ((e,_):_) = e
+
+subtractPoly :: [Term] -> [Term] -> [Term]
+subtractPoly f [] = f
+subtractPoly [] g = multiplyTermByPoly (0,(-1)) g
+subtractPoly ((e1,c1):f) ((e2,c2):g)
+  | e1 > e2 = (e1,c1) : subtractPoly f ((e2,c2):g)
+  | e1 < e2 = (e2,(-c2)) : subtractPoly ((e1,c1):f) g
+  | e1 == e2 = (e1,c1-c2) : subtractPoly f g
+
+subtractPolyMod :: Integer -> [Term] -> [Term] -> [Term]
+subtractPolyMod p f g = reducePolyMod p (subtractPoly f g)
+
+divideTermMod :: Integer -> Term -> Term -> Term
+divideTermMod p (e1,c1) (e2,c2) = (e1-e2,(c1 * inverseMod p c2) `mod` p)
+
+inverseMod :: Integer -> Integer -> Integer
+inverseMod p 1 = 1
+inverseMod p a = (n * p + 1) `div` a
+  where n = a - inverseMod a (p `mod` a)
+
+-- Modulus -> Divisor -> Dividend -> (Quotient,Remainder)
+polynomialDivideByMod :: Integer -> [Term] -> [Term] -> ([Term],[Term])
+polynomialDivideByMod p d f
+  | degree f < degree d = ([],f)
+  | otherwise = (thisq:subq,subr)
+    where
+      (subq,subr) = polynomialDivideByMod p d subf
+      subf = subtractPolyMod p f (multiplyTermByPolyMod p thisq d)
+      thisq = divideTermMod p (head f) (head d)
+
+polynomialGCDMod :: Integer -> [Term] -> [Term] -> [Term]
+polynomialGCDMod _ [] g = g
+polynomialGCDMod _ f [] = f
+polynomialGCDMod p f g = polynomialGCDMod p g h
+  where
+    (_,h) = polynomialDivideByMod p g f
+
+test_UnivariateGCD = [
+  reducePolyMod 3 [(18,6),(11,1),(9,(-8)),(2,(-1)),(0,5)] ~?=
+  [(11,1),(9,1),(2,2),(0,2)],
+  reducePolyMod 5 [(11,5),(8,100),(2,(-5)),(0,10)] ~?= [],
+  multiplyTerms (7,3) (5,4) ~?= (12,12),
+  multiplyTermByPoly (2,2) [(7,3),(5,8),(4,1),(3,11),(0,2)] ~?=
+  [(9,6),(7,16),(6,2),(5,22),(2,4)],
+  multiplyTermByPolyMod 13 (2,2) [(7,3),(5,8),(4,1),(3,11),(0,2)] ~?=
+  [(9,6),(7,3),(6,2),(5,9),(2,4)]
+  ]
