211f287352913bb25a1f3ccb15419b8f2967a508
Brian Johnson (Mac Pro) <bjthinks@gmail.com>
2010-11-02 01:18:43 -0700

    ExpressionDisplay.hs no longer has a life of its own

--- ExpressionDisplay.hs
+++ ExpressionDisplay.hs
@@ -6,35 +6,3 @@ import ASTDisplay
 
 displayExpr :: Expression -> String
 displayExpr = astDisplay . fromExpr
-
-{-
-import Expression
-import Data.Ratio
-
-displaySum :: [Expression] -> (String,Int)
-displaySum es =
-  (foldl withspace f fs',0) where
-    fs' = map expandSign fs
-    expandSign :: String -> String
-    expandSign ('-':cs) = '-':' ':cs
-    expandSign cs = '+':' ':cs
-    (f:fs) = map displayExpr constLastExprs
-    constLastExprs = filter (not . isRational) es ++ filter isRational es
-
-displayProduct :: [Expression] -> (String,Int)
-displayProduct es =
-  (minusSign ++ termsStr,1)
-  where
-    termsStr = case (numeratorTerms,denominatorTerms) of
-      (_,[]) -> numeratorStr
-      ([],_) -> "1 / " ++ denominatorStr
-      _ -> numeratorStr ++ " / " ++ denominatorStr
-    numeratorStr = termsToStr numeratorTerms
-    denominatorStr = termsToStr denominatorTerms
-    termsToStr = foldl1 withspace . map displayTerm
-    displayTerm t = parenthesize (displayWithPrecedence t) 1
-    (sign,numeratorTerms,denominatorTerms) = prodAsQuot es
-    minusSign = case sign of
-      (-1) -> "-"
-      _    -> ""
--}
