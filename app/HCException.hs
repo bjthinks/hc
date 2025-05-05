407a4240676d148a9fb1230d37265c92ea62bdca
Brian Johnson <bjthinks@gmail.com>
2025-05-05 10:24:19 -0700

    Unknown functions throw an HCException and no longer exit the program

--- app/HCException.hs
+++ app/HCException.hs
@@ -6,7 +6,8 @@ import Data.Typeable
 import Control.Exception
 
 data HCException = HCDivideByZero |
-                   HCNonIntegerPower
+                   HCNonIntegerPower |
+                   HCUnknownFunction String
                  deriving (Show, Typeable)
 
 instance Exception HCException
@@ -14,3 +15,4 @@ instance Exception HCException
 hcErrorMessage :: HCException -> String
 hcErrorMessage HCDivideByZero = "Error: division by zero"
 hcErrorMessage HCNonIntegerPower = "Error: exponents must be integers"
+hcErrorMessage (HCUnknownFunction f) = "Error: unknown function " ++ f
