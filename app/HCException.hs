f3e539f057efc869b35edf7419a57af2b86ae83b
Brian Johnson <bjthinks@gmail.com>
2025-05-28 21:41:42 -0700

    Remove HCUnknownFunction exception

--- app/HCException.hs
+++ app/HCException.hs
@@ -6,13 +6,11 @@ import Data.Typeable
 import Control.Exception
 
 data HCException = HCDivideByZero |
-                   HCNonIntegerPower |
-                   HCUnknownFunction String
-                 deriving (Show, Typeable)
+                   HCNonIntegerPower
+  deriving (Show, Typeable)
 
 instance Exception HCException
 
 hcErrorMessage :: HCException -> String
 hcErrorMessage HCDivideByZero = "Error: division by zero"
 hcErrorMessage HCNonIntegerPower = "Error: exponents must be integers"
-hcErrorMessage (HCUnknownFunction f) = "Error: unknown function " ++ f
