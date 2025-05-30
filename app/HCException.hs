880e520801c2853717fdd052ee36d8858221f8a3
Brian Johnson <bjthinks@gmail.com>
2025-05-29 21:40:59 -0700

    HCException error messages are complete sentences

--- app/HCException.hs
+++ app/HCException.hs
@@ -15,10 +15,10 @@ data HCException = HCDivideByZero |
 instance Exception HCException
 
 hcErrorMessage :: HCException -> String
-hcErrorMessage HCDivideByZero = "Error: division by zero"
-hcErrorMessage HCNonIntegerPower = "Error: exponents must be integers"
-hcErrorMessage (HCUnknownFunction f) = "Error: unknown function " ++ f
+hcErrorMessage HCDivideByZero = "Error: division by zero."
+hcErrorMessage HCNonIntegerPower = "Error: exponents must be integers."
+hcErrorMessage (HCUnknownFunction f) = "Error: unknown function " ++ f ++ "."
 hcErrorMessage HCSubstituteNotVariable = "Error: the first parameter to " ++
-  "substitute must be a variable"
+  "substitute must be a variable."
 hcErrorMessage (HCWrongNumberOfParameters f n) = "Error: function " ++ f ++
-  " takes " ++ show n ++ " parameter" ++ (if n /= 1 then "s" else "")
+  " takes " ++ show n ++ " parameter" ++ (if n /= 1 then "s" else "") ++ "."
